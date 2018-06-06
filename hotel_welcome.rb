10ac133d8ae244ed7f379c6984e786fe13ae2916
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-06 17:22:35 +0000

    automatically backed up by learn

---
+++ hotel_welcome.rb
@@ -0,0 +1,9 @@
+def tv_welcome(name, room_number)
+  
+  puts "welcome #{name.upcase}
+  puts "to the springhill suites"
+  puts "room #{room_number}"
+  
+end 
+
+tv_welcome("my name" , "113")
---
+++ loops.rb
@@ -0,0 +1,25 @@
+# def dance 
+#   moves_slay=0 
+
+#   loop do
+#   if moves_slay==10 
+#     break
+#   end
+#   puts "step to the left"
+#   puts "step to the right"
+#   puts "kick"
+#   moves_slay= moves_slay+1
+# end
+# end
+# dance
+
+def kwk
+  days = 2 
+#MAKES A WHILE loop 
+while days < 10 
+puts "keep coming to camp!!!"
+  #days = days + 1
+  days += 1 
+  end 
+end 
+
